2e33b2c86b428f60e7f7708cb0a832a61a00e75c
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2022-05-26 19:33:29 +0200

    added basic data importing

--- Borek Domaradzki Dziedzic P10.R
+++ Borek Domaradzki Dziedzic P10.R
@@ -2,12 +2,18 @@ setwd("C:/Users/ja/Documents/GitHub/Programowanie-w-R") #Stefan sciezka
 
 list.files()
 
-
-# 26.05.22 Lukasz Borek----
-
+# Importowanie danych ----
 
 
+# 26.05.22 Stefan
 
+platinum_raw    <- read.csv("TFT_Platinum_MatchData.csv")
+diamond_raw     <- read.csv("TFT_Diamond_MatchData.csv")
+master_raw      <- read.csv("TFT_Master_MatchData.csv")
+grandmaster_raw <- read.csv("TFT_Grandmaster_MatchData.csv")
+challenger_raw  <- read.csv("TFT_Challenger_MatchData.csv")
 
+items <- read.csv("items_id.csv")
+items <- items[,1]
 
-#koniec zmian----111
+# Stefan koniec zmian
