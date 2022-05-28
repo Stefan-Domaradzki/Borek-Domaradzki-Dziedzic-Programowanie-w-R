ffa48caa51b6f755ea9fa3a4079a8d4d08aa8c98
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2022-05-29 00:59:44 +0200

    próba zmiany id itemów na ich nazwy
    problem z wyciągnięciem jednoczenie jedno i dwucyfrowych liczb

--- Borek Domaradzki Dziedzic P10.R
+++ Borek Domaradzki Dziedzic P10.R
@@ -2,7 +2,14 @@ setwd("C:/Users/ja/Documents/GitHub/Programowanie-w-R") #Stefan sciezka
 
 list.files()
 
-# Importowanie danych ----
+install.packages("stringi")
+install.packages("tidyverse")
+
+
+library(stringi)
+library(tidyverse)
+
+# Importowanie i przygotowanie danych danych ----
 
 
 # 26.05.22 Stefan
@@ -25,3 +32,29 @@ items <- as.data.frame(items$item_id, row.names = rownames(items))
 
 
 # Stefan koniec zmian
+
+# 29.05.22 Stefan
+
+platinum_player_items    <- platinum_raw[,c(1,5,8)]
+diamond_player_items     <- diamond_raw[,c(1,5,8)]
+master_player_items      <- master_raw[,c(1,5,8)]
+grandmaster_player_items <- grandmaster_raw[,c(1,5,8)]
+challenger_player_items  <- challenger_raw[,c(1,5,8)]
+
+
+#pole testowe usunac
+a.1 <- platinum_player_items[c(1,2),]
+str(a.1)
+
+a.1 <- a.1 %>%
+  add_column(items = NA)
+
+a.1$champion <- stri_extract_all_regex(a.1$champion, "\\[.\\]")
+a.1$champion <- stri_extract_all_regex(a.1$champion, "\\[..\\]" | "\\[.\\]" )
+
+b.1 <- stri_trans_char('id.1', '1', as.character(items[1]))
+
+# Stefan koniec zmian 
+
+
+
